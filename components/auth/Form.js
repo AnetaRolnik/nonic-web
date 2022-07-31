1f84f23a307cb8feaaf6cd06c6ac1f2428f24a9d
Aneta Rolnik <aneta.r@poczta.onet.eu>
2022-07-31 18:26:04 +0200

    Use fetch to sign up user

--- components/auth/Form.js
+++ components/auth/Form.js
@@ -4,16 +4,35 @@ const Form = ({ signUpView }) => {
   const nameRef = useRef();
   const passwordRef = useRef();
   const mailRef = useRef();
+  const phoneRef = useRef();
 
   const formSubmitHandler = (event) => {
     event.preventDefault();
-
     const enteredName = nameRef.current.value;
     const eneteredPassword = passwordRef.current.value;
 
+    let body = {};
+
     if (signUpView) {
       const enteredMail = mailRef.current.value;
+      const eneteredPhone = phoneRef.current.value;
+
+      body = {
+        username: enteredName,
+        password: eneteredPassword,
+        email: enteredMail,
+        phone: eneteredPhone,
+      };
     }
+
+    fetch("http://localhost:8000/users/register/", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
   };
 
   return (
@@ -44,6 +63,15 @@ const Form = ({ signUpView }) => {
             name="mail"
             type="email"
             ref={mailRef}
+            required={true}
+            autoComplete="on"
+          />
+          <label htmlFor="phone">Numer telefonu</label>
+          <input
+            id="phone"
+            name="phone"
+            type="text"
+            ref={phoneRef}
             required={true}
             autoComplete="on"
           />
